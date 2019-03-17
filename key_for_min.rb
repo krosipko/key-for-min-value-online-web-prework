f2f5c3acd6800afd9cd5f3420a5a56d18bbd2cf3
Kyle Rosipko <krosipko@outlook.com>
2019-03-17 16:51:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,23 @@
   def key_for_min_value(name_hash = {})
 
   end
-  
-      lowest = ""
+
+  def key_for_min_value(name_hash = {})
+    lowest = ""
     temp = []
     name_hash.each do |key, value|
       temp << value
     end
-    cntr = 0
-    while cntr < temp.size
-    
+    temp.each do |value|
+      if temp[0] > temp[1]
+        temp.delete_at[0]
+      elsif temp[0] < temp[1]
+        temp.delete_at[1]
+      end
+    end
+    name_hash.each do |key, value|
+      if value == temp[0]
+        lowest = temp[0]
+      end
+    end
+  end

7da7fd3a4f6e4213b1e8f49ad759f2496d913bee
Kyle Rosipko <krosipko@outlook.com>
2019-03-17 17:02:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,11 @@
   def key_for_min_value(name_hash = {})
     loweset = ""
     temp = []
+    temp_sort = []
     name_hash.each do |key, value|
       temp << value
     end
-    temp
+    temp.each do |value|
+      if temp_sort.count
   end
 
